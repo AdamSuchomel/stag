8bd9dcea7365f14047621e8a329497b573134b19
suchoad1 <suchoad1@uhk.cz>
2025-04-07 16:21:03 +0200

    cc

--- src/main/java/pro1/Main2.java
+++ src/main/java/pro1/Main2.java
@@ -1,7 +1,10 @@
 package pro1;
 
 import com.google.gson.Gson;
+import pro1.apiDataModel.Action;
 import pro1.apiDataModel.ActionsList;
+
+import java.util.Comparator;
 
 public class Main2 {
     public static void main(String[] args) {
@@ -11,8 +14,14 @@ public class Main2 {
     public static long maxPersonsCount(String department, int year)
     {
         String json = Api.getActionsByDepartment(department,year);
-        ActionsList actions= new Gson().fromJson(json, ActionsList.class);
+        ActionsList actions = new Gson().fromJson(json, ActionsList.class);
 
-        return -1; // TODO 2.1: Vrať nejvyšší dosažený počet přihlášených studentů na jedné akci
+        // možnost 1
+        //return actions.items.stream().mapToLong(a -> a.personsCount).max().getAsLong();
+
+        //možnost 2 -> dostanu se přes stream k objektu s maximálním atributem personsCount
+        Action result = actions.items.stream().max(Comparator.comparing(a -> a.personsCount)).get();
+
+        return result.personsCount; // TODO 2.1: Vrať nejvyšší dosažený počet přihlášených studentů na jedné akci
     }
 }
--- src/main/java/pro1/Main4.java
+++ src/main/java/pro1/Main4.java
@@ -1,5 +1,12 @@
 package pro1;
 
+import com.google.gson.Gson;
+import pro1.apiDataModel.Teacher;
+import pro1.apiDataModel.TeachersList;
+
+import java.util.Comparator;
+import java.util.List;
+
 public class Main4 {
 
     public static void main(String[] args) {
@@ -8,6 +15,17 @@ public class Main4 {
 
     public static void printShortestEmails(String department, int count)
     {
+        String json2 = Api.getTeachersByDepartment(department);
+        TeachersList teachers = new Gson().fromJson(json2, TeachersList.class);
+
+        // uložení do listu, kde se nejdřív vyfiltruje, jestli ten učitel vůbec má email a potom se to seřadí (default podle nejmenšího) a tam se nastaví limit, kde do listu se dostane jen ten počet, co je v limitu.
+        List<Teacher> result = teachers.items.stream().filter(t->t.email != null).sorted(Comparator.comparing(t->t.email.length())).limit(count).toList();
+
         // TODO 4.1: Vypiš do konzole "count" nejkratších učitelských emailových adres
+        for(Teacher t : result)
+            System.out.println(t.email);
+
+        // 2. varianta -> forEach rovnou v tom
+        //teachers.items.stream().filter(t->t.email != null).sorted(Comparator.comparing(t->t.email.length())).limit(count).forEach(t->System.out.println(t.email));
     }
 }

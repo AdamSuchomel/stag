c0f62a5dfab4f750ef0dba1ddf0c1e611bf06714
Adam Suchomel <suchomel@wpj.cz>
2025-05-03 13:26:53 +0200

    test fixes

--- src/main/java/pro1/Main4.java
+++ src/main/java/pro1/Main4.java
@@ -15,13 +15,13 @@ public class Main4 {
 
     public static void printShortestEmails(String department, int count)
     {
+        // TODO 4.1: Vypiš do konzole "count" nejkratších učitelských emailových adres
         String json2 = Api.getTeachersByDepartment(department);
         TeachersList teachers = new Gson().fromJson(json2, TeachersList.class);
 
         // uložení do listu, kde se nejdřív vyfiltruje, jestli ten učitel vůbec má email a potom se to seřadí (default podle nejmenšího) a tam se nastaví limit, kde do listu se dostane jen ten počet, co je v limitu.
         List<Teacher> result = teachers.items.stream().filter(t->t.email != null).sorted(Comparator.comparing(t->t.email.length())).limit(count).toList();
 
-        // TODO 4.1: Vypiš do konzole "count" nejkratších učitelských emailových adres
         for(Teacher t : result)
             System.out.println(t.email);
 
